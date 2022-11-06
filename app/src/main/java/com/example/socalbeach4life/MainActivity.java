2755545be983ac67213762a4f530e3ccadeab0b2
Melanie Toh <melanie.toh@tecpal.com>
2022-11-06 01:19:03 -0700

    Front end work for activity_sign_up (signUpView)

--- app/src/main/java/com/example/socalbeach4life/MainActivity.java
+++ app/src/main/java/com/example/socalbeach4life/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.socalbeach4life;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
     /*
@@ -12,5 +18,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Clickable signUpLabel
+        final TextView signUpView = findViewById(R.id.signUpLabel);
+        signUpView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent switchToSignUpView = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(switchToSignUpView);
+            }
+        });
+    }
+
+    public void logIn(View view) {
+        // Retrieve user input
+        EditText emailField = findViewById(R.id.emailField);
+        EditText passwordField = findViewById(R.id.passwordField);
+        String email = emailField.getText().toString();
+        String password = passwordField.getText().toString();
+
+        // Send to database to validate
+        boolean isValidated = false;
+        // isValidated = validateAccount(email, password);
+        if(isValidated) { //
+            Intent switchToHomepageView = new Intent(MainActivity.this, HomepageActivity.class);
+            startActivity(switchToHomepageView);
+        }
+        else { // Log in failed error pop up + clear password field
+            // Pop up -> Cate
+            passwordField.setText("");
+        }
     }
 }
--- app/src/main/java/com/example/socalbeach4life/SignUpActivity.java
+++ app/src/main/java/com/example/socalbeach4life/SignUpActivity.java
@@ -2,7 +2,13 @@ package com.example.socalbeach4life;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class SignUpActivity extends AppCompatActivity {
     /*
@@ -12,5 +18,38 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        // Clickable signUpLabel
+        final TextView logInView = findViewById(R.id.logInLabel);
+        logInView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent switchToLogInView = new Intent(SignUpActivity.this, MainActivity.class);
+                startActivity(switchToLogInView);
+            }
+        });
+    }
+
+    public void signUp(View view) {
+        // Retrieve user input
+        EditText displayNameField = findViewById(R.id.displayNameField);
+        EditText emailField = findViewById(R.id.emailField);
+        EditText passwordField = findViewById(R.id.passwordField);
+        String displayName = displayNameField.getText().toString();
+        String email = emailField.getText().toString();
+        String password = passwordField.getText().toString();
+
+        // Send to database to create account
+        boolean hasCreated = false;
+        // hasCreated = createAccount(displayName, email, password);
+        if(hasCreated) { //
+            Intent switchToHomepageView = new Intent(SignUpActivity.this, HomepageActivity.class);
+            startActivity(switchToHomepageView);
+        }
+        else { // Sign up failed error pop up + clear fields
+            // Pop up -> Cate
+            displayNameField.setText("");
+            emailField.setText("");
+            passwordField.setText("");
+        }
     }
 }
