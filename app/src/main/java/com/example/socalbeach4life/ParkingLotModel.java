0120b5ac52a7b589c6c4df4ab25433c8c21321f2
Catriona Jung <cyjung@usc.edu>
2022-11-07 15:47:38 -0800

    Functioning Parking Lots Map Activity and updated Beach/Parking Lot models

--- app/src/main/java/com/example/socalbeach4life/ParkingLotModel.java
+++ app/src/main/java/com/example/socalbeach4life/ParkingLotModel.java
@@ -1,8 +1,12 @@
 package com.example.socalbeach4life;
+import java.io.Serializable;
 
-public class ParkingLotModel {
+public class ParkingLotModel implements Serializable {
     public String name;
     public String coord;
+    public String address;
+    public Double lat;
+    public Double lon;
     public Double distance;
 
     public String getName() {
@@ -17,15 +21,25 @@ public class ParkingLotModel {
         return distance;
     }
 
-    public ParkingLotModel(String name, String coord, Double distance) {
+    public Double getLat() { return lat; }
+
+    public Double getLon() { return lon; }
+
+    public ParkingLotModel(String name, String address, Double distance, Double lat, Double lon) {
         this.name = name;
-        this.coord = coord;
+        this.address = address;
+        // this.coord = coord;
         this.distance = distance;
+        this.lat = lat;
+        this.lon = lon;
     }
 
     public ParkingLotModel() {
         this.name = "error";
         this.coord = "error";
+        this.address = "error";
         this.distance = 0.0;
+        this.lat = 0.0;
+        this.lon = 0.0;
     }
 }
