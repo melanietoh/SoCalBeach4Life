3d3965fc4faf8b45a042d46c84996302b672d6c7
Eric Cheng <eric.bc8@gmail.com>
2022-11-07 04:38:53 -0800

    ReviewModel Update

--- app/src/main/java/com/example/socalbeach4life/ReviewModel.java
+++ app/src/main/java/com/example/socalbeach4life/ReviewModel.java
@@ -1,27 +1,27 @@
 package com.example.socalbeach4life;
 
 public class ReviewModel {
-    public String uid;
+    public String username;
     public String displayName;
     public String message;
     public Double rating;
 
     public ReviewModel(String username, String displayName, String message, Double rating) {
-        this.uid = username;
+        this.username = username;
         this.displayName = displayName;
         this.message = message;
         this.rating = rating;
     }
 
     public ReviewModel() {
-        this.uid = "error";
+        this.username = "error";
         this.displayName = "error";
         this.message = "error";
         this.rating = 0.0;
     }
 
-    public String getUid() {
-        return uid;
+    public String getUsername() {
+        return username;
     }
 
     public String getDisplayName() {
