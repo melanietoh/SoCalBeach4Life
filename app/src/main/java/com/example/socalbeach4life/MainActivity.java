04f643583d4bb889380afb9d945e2bec41b56d23
Melanie Toh <melaniex@usc.edu>
2022-11-07 17:44:44 -0800

    Database calls for user reg and log in

--- app/src/main/java/com/example/socalbeach4life/MainActivity.java
+++ app/src/main/java/com/example/socalbeach4life/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.socalbeach4life;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
@@ -8,7 +9,12 @@ import android.widget.EditText;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     /*
@@ -36,16 +42,34 @@ public class MainActivity extends AppCompatActivity {
         String email = emailField.getText().toString();
         String password = passwordField.getText().toString();
 
-        // Send to database to validate
-        boolean isValidated = false;
-        // isValidated = validateAccount(email, password);
-        if(isValidated) { //
-            Intent switchToHomepageView = new Intent(this, HomepageActivity.class);
-            startActivity(switchToHomepageView);
-        }
-        else { // Log in failed error pop up + clear password field
-            // Pop up -> Cate
-            passwordField.setText("");
-        }
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task)
+            {
+                if (task.isSuccessful()) {
+                    System.out.println(getApplicationContext() + "Registration successful!");
+
+                    // if the user created intent to login activity
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    System.out.println(user.getDisplayName());
+                    System.out.println(user.getEmail());
+                    System.out.println(user.getUid());
+
+                    Intent switchToHomepageView = new Intent(MainActivity.this, BeachMapsActivity.class);
+                    startActivity(switchToHomepageView);
+                }
+                else {
+
+                    // Registration failed
+                    System.out.println(task.getException());
+                    System.out.println(
+                            getApplicationContext() +
+                                    " Login failed!!"
+                                    + " Please try again later");
+                    passwordField.setText("");
+                }
+            }
+        });
     }
 }
