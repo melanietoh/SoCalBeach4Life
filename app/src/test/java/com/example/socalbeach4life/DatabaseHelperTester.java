bd756400c5926a55a58900f4795e6746625ae4c8
Eric Cheng <eric.bc8@gmail.com>
2022-12-02 15:37:17 -0800

    gmaps api update

--- app/src/test/java/com/example/socalbeach4life/DatabaseHelperTester.java
+++ app/src/test/java/com/example/socalbeach4life/DatabaseHelperTester.java
@@ -4,6 +4,8 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import android.text.style.TtsSpan;
+
 public class DatabaseHelperTester {
 
     /** Setup method executed before each test */
@@ -31,7 +33,7 @@ public class DatabaseHelperTester {
 
     @Test
     public void testCoordParsing() {
-        String address1 = "2600 Highland AveManhattan Beach, CA 90266";
+        String address1 = "2600 Highland Ave Manhattan Beach, CA 90266";
         String address2 = "2114 Highland Ave Manhattan Beach, CA 90266";
         assertFalse("address 1 is not waypoint", DatabaseHelper.isWaypoint(address1));
         assertFalse("address 2 is not waypoint", DatabaseHelper.isWaypoint(address2));
@@ -40,6 +42,14 @@ public class DatabaseHelperTester {
         assertTrue("waypoint 1", DatabaseHelper.isWaypoint(waypoint1));
         assertTrue("waypoint 2", DatabaseHelper.isWaypoint(waypoint2));
 
+    }
+
+    @Test
+    public void testMultiStop() {
+        String address1 = "2600 Highland Ave Manhattan Beach, CA 90266";
+        String waypoint1 = "33.848602407888734,-118.3976579056785";
+        System.out.println(DatabaseHelper.generateTwoPartRouteWithCustomStart(DatabaseHelper.USCADDRESS, address1,waypoint1));
+        System.out.println(DatabaseHelper.generateTwoPartRouteFromCurrentLocation(address1, waypoint1));
     }
 
 }
